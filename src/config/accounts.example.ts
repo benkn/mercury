764e1b1399ab4f99e76dca4375469115386ab6a6
Ben Knear <ben.knear@gmail.com>
2023-12-23 12:05:02 -0500

    chore: make it more clear this is a fake credential

--- src/config/accounts.example.ts
+++ src/config/accounts.example.ts
@@ -6,11 +6,11 @@ export const accounts: Array<Account> = [
   {
     name: 'Bank of Nottingham',
     item_id: 'akfwjklsdDKDKl39dkDid8D',
-    access_token: 'access-production-85225934-a0ee-4405-b141-a87d8edc92d0',
+    access_token: 'not-real-85225934-a0ee-4405-b141-a87d8edc92d0',
   },
   {
     name: 'Sherwood National Bank',
     item_id: 'Yy5BQl5nvUmBllBQooEaQA',
-    access_token: 'access-production-699db5a5-b629-4777-9b79-129f96884ebf',
+    access_token: 'not-real-699db5a5-b629-4777-9b79-129f96884ebf',
   }
 ]
